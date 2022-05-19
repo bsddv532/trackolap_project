978acb2216696f66d080ce9aed4fb4f28f20cf81
bsddv532 <deepakpradhan9962@gmail.com>
2022-05-19 11:28:35 +0530

    TRACKOLAP CRUD OPERATION ASSIGNMENT

---
+++ src/Components/EmployeeContext.js
@@ -0,0 +1,14 @@
+import React, { useState, createContext } from 'react'
+
+export const EmployeeContext = createContext();
+
+export const EmployeeContextProvider = (props) => {
+
+    const [employeeData, setEmployeeData] = useState([]);
+
+  return (
+        <EmployeeContext.Provider value= {[employeeData, setEmployeeData] }>
+            {props.children}
+        </EmployeeContext.Provider>
+  )
+}
